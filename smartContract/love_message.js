ee34e0e7aa25a21a962d3146f1a8818197e901ef
wutianlong <804098948@qq.com>
2018-05-09 16:35:08 +0800

    base nebuluas blockchain  develop dapp , first commit source codes

---
+++ smartContract/love_message.js
@@ -0,0 +1,71 @@
+"use strict";
+
+var DictItem = function(text) {
+	if (text) {
+		var obj = JSON.parse(text);
+		this.key = obj.key;
+		this.value = obj.value;
+		this.author = obj.author;
+	} else {
+	    this.key = "";
+	    this.author = "";
+	    this.value = "";
+	}
+};
+
+DictItem.prototype = {
+	toString: function () {
+		return JSON.stringify(this);
+	}
+};
+
+var LoveMessage = function () {
+    LocalContractStorage.defineMapProperty(this, "repo", {
+        parse: function (text) {
+            return new DictItem(text);
+        },
+        stringify: function (o) {
+            return o.toString();
+        }
+    });
+};
+
+LoveMessage.prototype = {
+    init: function () {
+        // todo
+    },
+
+    save: function (key, value) {
+
+        key = key.trim();
+        value = value.trim();
+        if (key === "" || value === ""){
+            throw new Error("empty key / value");
+        }
+        if (value.length > 64 || key.length > 64){
+            throw new Error("爱的关键词 或 爱的寄语 超过长度限制。。")
+        }
+
+        var from = Blockchain.transaction.from;
+        var dictItem = this.repo.get(key);
+        if (dictItem){
+            throw new Error("爱的关键词已经被占用，请重新输入。。");
+        }
+
+        dictItem = new DictItem();
+        dictItem.author = from;
+        dictItem.key = key;
+        dictItem.value = value;
+
+        this.repo.put(key, dictItem);
+    },
+
+    get: function (key) {
+        key = key.trim();
+        if ( key === "" ) {
+            throw new Error("输入 爱的关键词 为空，请重新输入！")
+        }
+        return this.repo.get(key);
+    }
+};
+module.exports = LoveMessage;
